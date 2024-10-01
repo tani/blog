db9bc6c4352d38117c538fe990d588d1a053683e
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-01 11:30:15 +0000

    remove tags

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
 import Footer from "../components/footer.vue";
 import Profile from "../components/profile.vue";
-import Tags from "../components/tags.vue";
 </script>
 <template>
-	<div class="w-[fit-content] mx-auto">
-		<Header />
-		<div class="flex justify-center gap-4">
-			<main class="prose prose-zinc">
-				<h2>記事一覧</h2>
-				<ContentList v-slot="{ list }">
-					<article v-for="article in list" :key="article._path">
-						{{ article.title }}
-						{{ article.description }}
-					</article>
-				</ContentList>
-			</main>
-			<aside class="w-[20ch]">
-				<section>
-					<Profile />
-				</section>
-				<section>
-					<Tags />
-				</section>
-			</aside>
-		</div>
-		<Footer />
-	</div>
+  <div class="w-[fit-content] mx-auto">
+    <Header />
+    <div class="flex gap-4">
+      <main class="prose prose-zinc">
+        <h2>記事一覧</h2>
+        <content-list v-slot="{ list }">
+          <article v-for="article in list" :key="article._path">
+            <nuxt-link :to="article._path">
+              {{ article.title }} 
+            </nuxt-link>
+            <p>
+              {{ article.description }}
+            </p>
+          </article>
+        </content-list>
+      </main>
+      <aside class="w-[20ch]">
+        <section>
+          <Profile />
+        </section>
+      </aside>
+    </div>
+    <Footer />
+  </div>
 </template>
