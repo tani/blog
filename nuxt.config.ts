c8a6cec6f424fec93f289bfba4b8c12a34a5e204
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-01 15:43:25 +0000

    Remove @nuxtjs/seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   site: {
     url: "https://tani.github.io",
   },
-  modules: ["@nuxt/content", "@unocss/nuxt", "@nuxtjs/seo", "@nuxt/image"],
+  modules: ["@nuxt/content", "@unocss/nuxt", "@nuxt/image"],
   content: {
     markdown: {
       remarkPlugins: ["remark-math"],
